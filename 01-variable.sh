cb5285902cd304ba5a8649dcfacdf82476eec863
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-13 12:02:33 +0530

    variable

--- 01-variable.sh
+++ 01-variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1:mahesh
-PERSON2:rajesh
+PERSON1=mahesh
+PERSON2=rajesh
 
 echo "$PERSON1:: hii $PERSON2,how r u"
 echo "$PERSON2:: hii $PERSON1 , i am very good how about you"

df1c94ed67287f2bd750bdf297c5a78bc9faf8dd
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-13 16:47:04 +0530

    variable

--- install -mysql.sh
+++ install -mysql.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 ID=$(id -u)
-
+TIMESTAMP=$(date+%F.%H:%M:%S)
+echo "script name: $0"
+VALIDATE( ){
+if [ $1 -ne 0 ]
+then 
+echo "ERROR :: $2 ....FAILED"
+exit 1
+else 
+echo"$2 ...SUCESS"
+fi
+}
 if [ $ID -ne 0 ]
 then 
    echo "error :please run with root user"
@@ -8,28 +18,4 @@ then
 fi
 
 yum install mysql -y
-
-if [ $? -ne 0 ]
- 
- then 
- echo "error : package installing error"
-exit 1
-else 
-
-echo " package installed succesfully"
-
-fi
-
-
 yum install git -y
-
-if [ $? -ne 0 ]
-
-then 
-echo "error :: git instalation is failed"
-exit 1
-else 
-
-echo " git install is sucdesfully we will cheer up"
-
-fi

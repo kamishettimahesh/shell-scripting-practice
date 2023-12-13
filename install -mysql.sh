74890aeba20fd122cf69bbe8bae3161517cafb28
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-13 16:24:01 +0530

    variable

--- install -mysql.sh
+++ install -mysql.sh
@@ -19,3 +19,17 @@ else
 echo " package installed succesfully"
 
 fi
+
+
+yum install git -y
+
+if [ $? -ne 0 ]
+
+then 
+echo "error :: git instalation is failed"
+exit 1
+else 
+
+echo " git install is sucdesfully we will cheer up"
+
+fi

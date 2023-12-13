48b75c6741a9e12b0c5c5c8fa38c489271fed52b
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-13 12:12:02 +0530

    variable

--- 01-variable.sh
+++ 01-variable.sh
@@ -6,3 +6,8 @@ PERSON2=RAHEEM
 echo "$PERSON1:: hii $PERSON2,how r u"
 echo "$PERSON2:: hii $PERSON1 , i am very good how about you"
 echo "$PERSON1:: i am very good $PERSON2"
+
+
+DATE=$(date)
+
+echo "time and date is ${DATE}"

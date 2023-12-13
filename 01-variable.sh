3c0726d1af890a6043c5b88748fee4104166efca
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-13 12:25:38 +0530

    variable

--- 01-variable.sh
+++ 01-variable.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-PERSON1=RAM
-PERSON2=RAHEEM
+PERSON1=$1
+PERSON2=$2
 
-echo "$PERSON1:: hii $PERSON2,how r u"
-echo "$PERSON2:: hii $PERSON1 , i am very good how about you"
-echo "$PERSON1:: i am very good $PERSON2"
+echo "$PERSON1: Hii $PERSON2,how r u"
+echo "$PERSON2: Hii $PERSON1 , I am very good how about you"
+echo "$PERSON1: I am very good $PERSON2"
+echo "$PERSON2: TYPING ......."
 
 
 DATE=$(date)

c995ed411ed025d1ac73e7d6b6a0dd43da6aa894
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-13 12:01:36 +0530

    variable

--- 01-variable.sh
+++ 01-variable.sh
@@ -3,6 +3,6 @@
 PERSON1:mahesh
 PERSON2:rajesh
 
-echo "$PERSON1 :: hii $PERSON2,how r u"
-echo "$PERSON2 :: hii $PERSON1 , i am very good how about you"
-echo "$PERSON1 :: i am very good $PERSON2"
+echo "$PERSON1:: hii $PERSON2,how r u"
+echo "$PERSON2:: hii $PERSON1 , i am very good how about you"
+echo "$PERSON1:: i am very good $PERSON2"

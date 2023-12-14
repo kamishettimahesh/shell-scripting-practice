5ec268fa2dd10ce3a474b83c5c1625cc44aa334c
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-14 13:14:06 +0530

    shell script

--- mysql.sh
+++ mysql.sh
@@ -11,24 +11,27 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE () {
 
-# shellcheck disable=SC2086
 if [ $1 -ne 0 ]
 
 then
    echo -e "$2 .... $R failed $N"
-   exit 13
+   exit 1
 else
-  echo -e "$2 .... $G success $N"
+   echo -e "$2 .... $G success $N"
 fi
 }
 
-if [ "$ID" -ne 0 ]
+if [ $ID -ne 0 ]
 
 then 
-    echo -e "$R Error:: please run with root user $N"
-    exit 13
-else 
+    echo -e "$R Error::please run with root user $N"
+
+    exit 1
+
+else
+
    echo "you are root user"
+
 fi
 
 yum install mysql -y &>> $LOGFILE

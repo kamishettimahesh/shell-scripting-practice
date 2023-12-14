a114c51eabed26b4247f29fc20f75ff3b5d349dc
kamishettimahesh <maheshkamishetti15@gmail.com>
2023-12-14 12:35:27 +0530

    shell script

--- mysql.sh
+++ mysql.sh
@@ -10,26 +10,22 @@ TIMESTAMP=$(date +%F.%H:%M:%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE () {
+
 if [ $1 -ne 0 ]
+
 then
-  echo -e " $2 .... $R  failed $N"
-    exit1
+   echo -e "$2 .... $R failed $N"
+   exit 1
 else
-
-
-    echo -e "$2 .... $G success $N"
-
+  echo -e "$2 .... $G success $N"
 fi
-
 }
 if [ $ID -ne 0 ]
 then 
     echo "$R Error:please run with root user $N"
-    
-     exit1
+    exit 1
 else 
    echo "you are root user"
-
 fi
 
 yum install mysql -y &>> $LOGFILE
